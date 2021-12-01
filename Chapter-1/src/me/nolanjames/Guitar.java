7a1a9263ee8c9edeb4cfe28315cf8be71998c898
Nolan-James <nj@nolanjames.me>
2021-12-01 10:39:54 +0000

    Chapter 1 complete

---
+++ Chapter-1/src/me/nolanjames/Guitar.java
@@ -0,0 +1,33 @@
+package me.nolanjames;
+
+public class Guitar {
+
+    private String serialNumber;
+    private double price;
+    private GuitarSpec guitarSpec;
+
+    public Guitar(String serialNumber, double price,
+                  GuitarSpec guitarSpec) {
+        this.serialNumber = serialNumber;
+        this.price = price;
+        this.guitarSpec = guitarSpec;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public GuitarSpec getGuitarSpec() {
+        return guitarSpec;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+}
---
+++ Chapter-1/src/me/nolanjames/Type.java
@@ -0,0 +1,13 @@
+package me.nolanjames;
+
+public enum Type {
+    ACOUSTIC, ELECTRIC;
+
+    public String toString() {
+        switch(this) {
+            case ACOUSTIC: return "acoustic";
+            case ELECTRIC: return "electric";
+            default:       return "unspecified";
+        }
+    }
+}
